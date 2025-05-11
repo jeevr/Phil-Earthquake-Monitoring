1bfe094db7e65daf41bc36f5c49c7202f5de4a1a
jlendio <jlendio@infrashieldlabs.com>
2025-05-12 00:25:45 +0800

    database update

--- Database Scripts/01 schema/create table - tblearthquake_data_daily.sql
+++ Database Scripts/01 schema/create table - tblearthquake_data_daily.sql
@@ -1,6 +1,37 @@
 
+SELECT name AS schema_name
+FROM sys.schemas
+ORDER BY name;
+
+-- raw table
+
+
+
+
+
 -- drop table if exists tblearthquake_data_daily
-create table tblearthquake_data_daily (
+create table raw.tblearthquake_data_daily (
+    date_time varchar,
+    date varchar,
+    time varchar,
+    latitude varchar,
+    longitude varchar,
+    depth_km varchar,
+    depth_km_symbol varchar,
+    magnitude varchar,
+    location varchar,
+    hlink varchar,
+    details varchar
+);
+
+
+select *
+from raw.tblearthquake_data_daily
+
+
+
+-- drop table if exists tblearthquake_data_daily
+create table fnl.tblearthquake_data_daily (
     date date,
     time time,
     geo_lat double precision,
@@ -17,3 +48,6 @@ create table tblearthquake_data_daily (
     expecting_aftershocks varchar,
     page_link varchar
 )
+
+select *
+from fnl.tblearthquake_data_daily
